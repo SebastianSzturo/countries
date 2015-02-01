add6309e824c47543c7fd0c4060497a1832bff01
Sebastian Szturo <s.szturo@me.com>
2015-02-01 22:35:48 +0900

    Add Information for hex

--- mix.exs
+++ mix.exs
@@ -1,11 +1,17 @@
 defmodule Countries.Mixfile do
   use Mix.Project
   
+  @version "1.0.0"
+  
   def project do
-    [app: :country,
-     version: "0.0.1",
+    [app: :countries,
+     version: @version,
      elixir: "~> 1.0",
-     deps: deps]
+     deps: deps,
+     
+     # Hex
+     description: description,
+     package: package]
   end
   
   def application do
@@ -14,5 +20,17 @@ defmodule Countries.Mixfile do
   
   defp deps do
     [{:yamerl, git: "https://github.com/yakaz/yamerl.git"}]
+  end
+  
+  defp description do
+    """
+    Countries is a collection of all sorts of useful information for every country in the [ISO 3166](https://de.wikipedia.org/wiki/ISO_3166) standard.
+    """
+  end
+  
+  defp package do
+    [contributors: ["Sebastian Szturo"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/SebastianSzturo/countries"}]
   end
 end
