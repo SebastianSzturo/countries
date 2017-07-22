c79117b84925960a8ac20991c354005eb61b3ad4
Marcel Otto <marcelotto@gmx.de>
2017-07-23 00:23:54 +0200

    Add conversion of char lists to strings and :null to nil (#17)
    and some refactorings for more idiomatic code

---
+++ lib/loader.ex
@@ -0,0 +1,57 @@
+defmodule Countries.Loader do
+  @moduledoc false
+
+  alias Countries.Country
+
+  @data_path Path.expand("data", __DIR__)
+
+  # loads all country data from our yaml files
+  # :yamerl returns a really terrible data structure
+  #    [[{'name', 'Germany'}, {'code', 'DE'}], [{'nam:e', 'Austria'}, {'code', 'AT'}]]
+  # We need to transform that to maps:
+  #    [%{name: "Germany', code: "DE"}, %{name: 'Austria', code: "AT"}]
+  def load do
+    data_path("countries.yaml")
+    |> :yamerl.decode_file
+    |> List.first
+    |> Enum.flat_map(fn code ->
+         :yamerl.decode_file data_path("countries/#{code}.yaml")
+       end)
+    |> Enum.map(&convert_country/1)
+  end
+
+  defp data_path(path) do
+    Path.join(@data_path, path)
+  end
+
+  defp convert_country(country_data) do
+     Enum.reduce(country_data, %Country{}, fn({attribute, value}, country) ->
+       with attribute = List.to_atom(attribute) do
+         Map.put(country, attribute, convert_value(attribute, value))
+       end
+     end)
+  end
+
+  @do_not_convert ~w[national_number_lengths national_destination_code_lengths]a
+
+  defp convert_value(_, :null),
+    do: nil
+
+  defp convert_value(:vat_rates, vat_rates) when not is_nil(vat_rates) do
+    Map.new(vat_rates, fn
+      {key, :null}       -> {List.to_atom(key), nil}
+      {key, value}       -> {List.to_atom(key), value}
+    end)
+  end
+
+  defp convert_value(:names, names),
+    do: Enum.map(names, &to_string/1)
+
+  defp convert_value(attribute, value)
+    when is_list(value) and not attribute in @do_not_convert,
+    do: to_string(value)
+
+  defp convert_value(_, value),
+    do: value
+
+end
