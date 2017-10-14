36f325637d22f44cc2c372f72bbe94ed665b0c6d
Sebastian Szturo <sebastian.szturo@gmail.com>
2017-10-14 11:30:32 +0900

    Revert "Fix compilation errors (#22)"
    This reverts commit cd2fcff2f8c6efb543893f8efbca3c8600829997.

--- lib/subdivision.ex
+++
@@ -1,3 +0,0 @@
-defmodule Countries.Subdivision do
-  defstruct [:id, :name, :unofficial_names, :translations, :geo]
-end
--- lib/utils.ex
+++
@@ -1,8 +0,0 @@
-defmodule Countries.Utils do
-  def to_map([item | _] = values) when is_tuple(item),
-    do: Enum.reduce(values, Map.new, fn ({key, value}, acc) ->
-      Map.put(acc, key |> to_string() |> String.to_atom(), to_map(value))
-    end)
-  def to_map(value) when is_list(value), do: to_string(value)
-  def to_map(value), do: value
-end
